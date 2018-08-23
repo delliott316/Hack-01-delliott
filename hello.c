f6bd37445f5a0b70626d6f7dfacf5d0a84407664
delliott316 <42649878+delliott316@users.noreply.github.com>
2018-08-23 11:45:51 -0500

    Hack1Dalton
    Changed it from the original lab text to this text.

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Dalton Elliott
+ * Date: 2018/23/08
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Dalton Elliott Mechanical Engineering!\n");
+
+  return 0;
+}
